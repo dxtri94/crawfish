b10d5ea809b73e965df19015387a49b9dd2f762a
Tri Doan <dxtri94@gmail.com>
2017-03-24 09:37:31 +0700

    Core web

---
+++ src/header/header.js
@@ -0,0 +1,14 @@
+angular.module('app.header', [])
+
+  .controller('HeaderController',
+    function ($scope) {
+
+      // actions
+      $scope.actions = {
+        init: function () {
+          // TODO
+        }
+      };
+      $scope.actions.init();
+    }
+  );
